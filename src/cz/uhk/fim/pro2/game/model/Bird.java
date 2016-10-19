ced0932b9ee139b48ce84444294d9ca570b96514
chvatlu1 <chvatlu1@J09-02.uhk.cz>
2016-10-19 16:25:37 +0200

    Prvotní model hry

---
+++ src/cz/uhk/fim/pro2/game/model/Bird.java
@@ -0,0 +1,44 @@
+package cz.uhk.fim.pro2.game.model;
+
+public class Bird {
+	private float positionX, positionY;
+	private float speed;
+	private int lives;
+	private String name;
+	
+	public void goUp() {
+		
+	}
+	
+	public void catchHeart() {
+		
+	}
+	
+	public void die() {
+		
+	}
+	
+	public void addLive() {
+		
+	}
+	
+	public void removeLive() {
+		
+	}
+
+	public float getPositionX() {
+		return positionX;
+	}
+
+	public void setPositionX(float positionX) {
+		this.positionX = positionX;
+	}
+	
+	public float getPositionY() {
+		return positionY;
+	}
+
+	public void setPositionY(float positionY) {
+		this.positionY = positionY;
+	}
+}
---
+++ src/cz/uhk/fim/pro2/game/model/Heart.java
@@ -0,0 +1,21 @@
+package cz.uhk.fim.pro2.game.model;
+
+public class Heart {
+	private float positionX, positionY;
+
+	public float getPositionX() {
+		return positionX;
+	}
+
+	public void setPositionX(float positionX) {
+		this.positionX = positionX;
+	}
+
+	public float getPositionY() {
+		return positionY;
+	}
+
+	public void setPositionY(float positionY) {
+		this.positionY = positionY;
+	}
+}
