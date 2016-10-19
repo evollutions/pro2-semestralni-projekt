a6aca913c45a90ecb7625ae194f610302e5ae374
chvatlu1 <chvatlu1@J09-02.uhk.cz>
2016-10-19 16:05:10 +0200

    První commit

---
+++ src/cz/uhk/fim/pro2/semestralni_projekt/Bird.java
@@ -0,0 +1,32 @@
+package cz.uhk.fim.pro2.semestralni_projekt;
+
+public class Bird {
+	private int liveCount;
+	
+	private float speed;
+	private String name;
+	
+	public float getSpeed() {
+		return speed;
+	}
+	
+	public void setSpeed(float speed) {
+		this.speed = speed;
+	}
+	
+	public int getLiveCount() {
+		return liveCount;
+	}
+	
+	public void setLiveCount(int liveCount) {
+		this.liveCount = liveCount;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ src/cz/uhk/fim/pro2/semestralni_projekt/Main.java
@@ -0,0 +1,9 @@
+package cz.uhk.fim.pro2.semestralni_projekt;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+}
